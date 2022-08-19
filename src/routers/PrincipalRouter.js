cbb04c9f6be711e35920169f06157efc0971f5e9
Manuimfeld <manu_jadir@hotmail.com>
2022-08-19 14:49:19 -0300

    Add Home component and Route home

--- src/routers/PrincipalRouter.js
+++ src/routers/PrincipalRouter.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "../components/Login";
+import Home from "../components/Home";
 
 const PrincipalRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/inicio" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
