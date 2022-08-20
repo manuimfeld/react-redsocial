023be28ff40d1d2be7c3b2b1c68214366cce73d3
Manuimfeld <manu_jadir@hotmail.com>
2022-08-19 23:13:51 -0300

    Insert Header component in PrincipalRouter

--- src/routers/PrincipalRouter.js
+++ src/routers/PrincipalRouter.js
@@ -2,10 +2,12 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "../components/Login";
 import Home from "../components/Home";
+import Header from "../components/Header";
 
 const PrincipalRouter = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/inicio" element={<Home />} />
